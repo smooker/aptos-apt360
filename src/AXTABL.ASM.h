c7ea711f951ba899ff01cc331edf80cc05e1151f
Brent Muller <brent.g.muller@gmail.com>
2015-12-13 22:13:08 -0700

    changed a long long int definition to uinteger mistakenly; removed some blank lines; the ADFPRL.ASM.C file was not linking properly without a named struct for some reason.

--- src/AXTABL.ASM.h
+++ src/AXTABL.ASM.h
@@ -1,36 +1,20 @@
 //first number: class
-
 //second: 	sub-class
-
 //last:		protap
-
 #ifndef AXTABL_H
-
 #define AXTABL_H
-
 #include "f2c.h"
 
-
-
 struct ClassRef{
-
 	shortint classId;
-
 	shortint subClass;
-
 	shortint proTapSubClass;
-
 	shortint proTapClass;
 };
 
-
-
 struct Keyword{
-
 	char* word;
-
 	struct ClassRef data;
-
 };
 
 #endif
